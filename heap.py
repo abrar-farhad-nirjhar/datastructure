2e04248e0e9bd1e0ee94e3ee73dded5615ffcd5d
sakib.alam <sakib.alam@skybettingandgaming.com>
2025-01-28 17:36:44 +0000

    no idea if this working

--- heap.py
+++ heap.py
@@ -12,13 +12,57 @@ class Node:
 class Heap:
     def __init__(self, root=None):
         self.root = root
-
     
-    def insert_node(self, node, order):
+    def insert_node(self, node):
         if not self.root:
+            print(f"setting root to node {node}")
             self.root = node
             return
-        
+
+        print(f"root is {self.root}")
+        order = [self.root]
+        while order:
+            current = order.pop(0)
+            print(f"looking at {current}")
+            if not current.left:
+                print(f"current has no left, setting {current} left to {node}")
+                current.left = node
+                break
+            if not current.right:
+                print(f"current has no right, setting {current} right to {node}")
+                current.right = node
+                break
+            order.append(current.left)
+            order.append(current.right)
+
+        self._bubble_up(node)
+
+
+    def _bubble_up(self, node):
+        while node:
+            print(f"bubbling up {node}")
+            parent = self._find_parent(node)
+            print(f"parent is {parent}")
+            if parent and parent.value > node.value:
+                print(f"swapping {parent} and {node}")
+                parent.value, node.value = node.value, parent.value
+                node = parent
+            else:
+                break
+
+    def _find_parent(self, node):
+        order = [self.root]
+        while order:
+            current = order.pop(0)
+            if current.left and current.left == node or current.right and current.right == node:
+                return current
+            if current.left:
+                order.append(current.left)
+            if current.right:
+                order.append(current.right)
+        return None
+
+
     def __iter__(self):
         order = [self.root]
         while order:
@@ -36,14 +80,18 @@ heap = Heap()
 for i in range(10):
     heap.insert_node(Node(i))
 
-heap.insert_node(Node(-1))
+heap.insert_node(Node(1.1))
+heap.insert_node(Node(1.3))
+
 
 def print_tree(node, level=0):
     if not node:
         return
-    
-    print_tree(node.right, level+1)
-    print(" "*level, node.value)
-    print_tree(node.left, level+1)
+
+    print_tree(node.right, level + 1)
+
+    print(" " * (4 * level) + f"{node.value}")
+
+    print_tree(node.left, level + 1)
 
 print_tree(heap.root)
